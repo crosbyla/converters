31038ffa08ece4b64d326b2f0b7f343e0384be8b
Lawrence Crosby <lawrence@Lawrences-MacBook-Pro.local>
2015-11-12 00:21:06 -0600

    fix write function to match C style

--- xyz2cif.py
+++ xyz2cif.py
@@ -7,17 +7,18 @@ def convert2cif(file_name):
     linelist = f.split('\n')
 
     outfile_name = file_name + "_coords.cif"
-    fp = open(file_name,'w')
+    fp = open(outfile_name,'w')
     for line in linelist[2:]:
         try :
             coords = zip((str,float,float,float),line.split()) 
             (elem, x, y, z) = [t(s) for t,s in coords] # extracts one line from file
             print(elem, x, y, z)
-            fp.write(elem, x, y, z)
+            fp.write("%s %1.6f %1.6f %1.6f \n" % elem, x, y, z)
         except ValueError:
             pass
 
     fp.close()
 #convert2cif(sys_name)
-def __main__ :
+def __main__ () :
+    file_name = 'STO-110.xyz'
     convert2cif(filename)

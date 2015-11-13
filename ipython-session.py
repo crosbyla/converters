769e9090d1141779bc76dec7dc398fe15a70e31e
crosbyla <crosbyla@u.northwestern.edu>
2015-11-12 23:00:12 -0600

    update cif converter with header

--- ipython-session.py
+++ ipython-session.py
@@ -1,69 +1,134 @@
 # coding: utf-8
-import numpy as np
-data = np.loadtxt('STO-110.xyz',skiprows=2, usecols=(1,2,3))
-trans = np.array([[0.707, -0.707, 0],[.707, .707, 0], [0, 0, 1]])
-data = np.dot(data,trans)
+testLine = ''.join('%s %1.6f %1.6f %1.6f' % (aelem[0], x[0], y[0], z[0]))
+get_ipython().magic('edit ipython-session.py')
 data
-np.savetxt('testout.txt', data, fmt='%1.6f')
-elem = np.loadtxt('STO-110.xyz',skiprows=2, usecols=(0,),dtype='S2')
-elem
-data
-np.savetxt('elemout2.txt', (elem,data), fmt='%s %1.6f')
-elem[0]
-data
-elem
-elem.shape
+x
+for a in data[:,0]:
+    print a
+    
+for a in data[:,0]:
+    print(a
+    )
+    
+for a in data[:,0]:
+    print(a)
+    
+for a,b,c  in data[:,0],data[:,1],data[:,2]:
+    print(a)
+    
+for a,b,c  in data[:,0],data[:,1],data[:,2]:
+    print(a,b,c)
+    
+data[:,0]
+data[:,2]
+data[:,3]
+data[:,2]
+data[:,1]
 data.shape
-a = elem + data
-np.rowstack()
-np.colstack()
-np.column_stack()
-np.row_stack()
-np.row_stack(elem,data)
-np.row_stack((elem,data))
-np.v_stack((elem,data))
-np.row_stack((elem,data))
-np.row_stack((data,elem))
-data.shape
-data.T
-np.row_stack(elem,data.T)
-np.row_stack((data,elem))
-np.row_stack((elem,data.T))
-_.shape
-testj = np.row_stack((elem,data.T))
-np.savetxt('test.txt', testj, fmt='%s %1.6f %1.6f %1.6f')
-np.savetxt('test.txt', testj, fmt='%s')
-get_ipython().system('cat test.txt')
-np.savetxt('test.txt', (elem, data), fmt='%s')
-np.savetxt('test.txt', (elem, data.T), fmt='%s')
-get_ipython().system('cat test.txt')
-np.savetxt('test.txt', (elem, data.T), fmt='%s %f')
-np.savetxt('test.txt', (elem, data.T), fmt='%s %f %f %f')
-np.savetxt('test.txt', (elem.T, data), fmt='%s %f %f %f')
-np.savetxt('test.txt', (elem, data.T), fmt='%s %f %f %f')
-np.savetxt('test.txt', (elem, data.T))
-(elem, data)
-(elem, data.T)
+for (a,b,c)  in data[:,0],data[:,1],data[:,2]:
+    print(a,b,c)
+    
+for (a,b,c)  in (data[:,0],data[:,1],data[:,2]):
+    print(a,b,c)
+    
+for i,a in enumerate(data[:,0]):
+    print i
+    
+for i,a in enumerate(data[:,0]):
+    print (i)
+    
+for i,a in enumerate(data[:,0]):
+    print (i,a)
+    
+for i in enumerate(data[:,0]):
+    print (i)
+    
+for i,a in enumerate(data[:,0]):
+    print (i)
+    
+for i,a in enumerate(data[:,0]):
+    print (data[i,:])
+    
+for i,a in enumerate(data[:,0]):
+    print (elem[i],data[i,:])
+    
+for i,a in enumerate(data[:,0]):
+    print (aelem[i],data[i,:])
+    
+for i,a in enumerate(data[:,0]):
+    print (aelem[i],data[i,:])
+    
+get_ipython().magic('edit ipython-session.py')
+
+employee_emails = ["Wile.E.Coyote@acme.com", 
+                   "Looney.Tunes@acme.com", 
+                   "Chuck.Jones@acme.com", 
+                   "Road.Runner@acme.com", 
+                   "Michael.Maltese@acme.com", 
+                   "Speedy.Gonzales@acme.com",
+                   "Calamity.Coyote@acme.com", 
+                   "Bugs.Bunny@texavery.com"]
+employee_ids = range(8)
+print employee_ids
+employee_emails = ["Wile.E.Coyote@acme.com", 
+                   "Looney.Tunes@acme.com", 
+                   "Chuck.Jones@acme.com", 
+                   "Road.Runner@acme.com", 
+                   "Michael.Maltese@acme.com", 
+                   "Speedy.Gonzales@acme.com",
+                   "Calamity.Coyote@acme.com", 
+                   "Bugs.Bunny@texavery.com"]
+employee_ids = range(8)
+print employee_ids
+aelem = np.genfromtxt('STO-110.xyz',skiprows=2, usecols=[0],dtype=np.str)
+alem
+aelem = np.genfromtxt('STO-110.xyz',skiprows=2, usecols=[0],dtype=np.str)
+aelem
+for i,a in enumerate(data[:,0]):
+    print (aelem[i],data[i,:])
+    
+for i,a in enumerate(data[:,0]):
+    print (aelem[i],data[i,1], data[i,2], data[i,3])
+    
+for i,a in enumerate(data[:,0]):
+    print (aelem[i],data[i,0], data[i,1], data[i,2])
+    
+open
+get_ipython().system('cat xyz2cif.py')
+for i,a in enumerate(data[:,0]):
+    print (elem[i],data[i,:])
+    
+for i,a in enumerate(data[:,0]):
+    print ('%s %1.10f' %elem[i],data[i,])
+    
+for i,a in enumerate(data[:,0]):
+    print ('%s %1.10f' % (elem[i],data[i,1]))
+    
+for i,a in enumerate(data[:,0]):
+    print ('%s %1.10f' % (aelem[i],data[i,1]))
+    
+import xyz2cifnp as xyz
+xyz.convert2cif('STO-110-Surf-O.xyz')
+import xyz2cifnp as xyz
+xyz.convert2cif('STO-110-Surf-O.xyz')
+del xyz
+import xyz2cifnp as xyz
+xyz.convert2cif('STO-110-Surf-O.xyz')
+get_ipython().system('cat STO-110-Surf-O.xyz')
+get_ipython().system('cat STO-110-Surf-O.xyz_coords.cif')
+xyz.convert2cif('STO-110-Surf-O.xyz')
+xyz.convert2cif('STO-110-Surf-O.xyz')
+del xyz
+import xyz2cifnp as xyz
+xyz.convert2cif('STO-110-Surf-O.xyz')
+data = np.loadtxt('STO-110-Surf-O.xyz', skiprows=2, usecols=(1,2,3))
 data
-np.invert(trans)
-trans
-np.linalg.inv(trans)
-itrans = np.linalg.inv(trans)
-np.dot(data,trans)
-elem,data
-f = np.zeros((38,4),dtype=('|S2,float64'))
-f
-f = np.zeros((38,),dtype=('|S2,float64, float64, float64'))
-f
-f[0]
-f[,0]
-f[:,0]
-f[:,]
-f[,:]
-f.shape
-np.vstack(data, elem)
-np.vstack((data, elem))
-np.vstack((data, elem.T))
-np.vstack((data.T, elem))
-_.T
-np.vstack((data, elem.T))
+data[:,0]
+del x, y, z
+del x
+del y
+del z
+xyz.convert2cif('STO-110-Surf-O.xyz')
+data [:,0]
+data [:,0].shape
+edata = np.genfromtxt('STO-110-Surf-O.xyz',skiprows=2, usecols=(0), dtype=str)
--- xyz2cifnp.py
+++ xyz2cifnp.py
@@ -4,7 +4,6 @@ import numpy as np
 from math import sin, cos, radians
 
 def convert2cif(file_name, rot=0):
-    print(file_name)
 
     elemArray = np.genfromtxt(file_name, skiprows=2, usecols=(0,), dtype=str)
     coordArray = np.loadtxt(file_name, skiprows=2, usecols=(1,2,3))
@@ -29,9 +28,26 @@ def convert2cif(file_name, rot=0):
     coordArray = np.dot(coordArray,trans)
 
     fp = open(outfile_name,'w')
+    print("data_"+file_name+"_phase", file=fp)
+    print("_symmetry_space_group_name_H-M   'P 1'", file=fp)
+
+    print("%s %f" % ( '_cell_length_a' , xlim , file=fp)
+    print("%s %f" % ( '_cell_length_b' , ylim , file=fp)
+    print("%s %f" % ( '_cell_length_c' , zlim , file=fp)
+
+    print("%s %f" % ( '_cell_angle_alpha' , 90 , file=fp)
+    print("%s %f" % ( '_cell_angle_alpha' , 90 , file=fp)
+    print("%s %f" % ( '_cell_angle_alpha' , 90 , file=fp)
+
+    print("loop_\n    _atom_site_type_symbol", file=fp)
+    print("    _atom_site_fract_x", file=fp)
+    print("    _atom_site_fract_y", file=fp)
+    print("    _atom_site_fract_z", file=fp)
+    print("    _atom_site_occupancy", file=fp)
 
     for i, elem in enumerate(elemArray[:]):
-        print("%s %1.6f %1.6f %1.6f" % (elem, x[i], y[i], z[i]), file=fp)
+        print("%s %1.6f %1.6f %1.6f %1.6f" % (elem, x[i], y[i], z[i], 1.0),
+            file=fp)
 
     fp.close()
 #convert2cif(sys_name)

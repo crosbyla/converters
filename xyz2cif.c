aeae93b5d6e2d75c688ba25454a83d8a70c337de
Lawrence Crosby <lawrence@Lawrences-MacBook-Pro.local>
2015-11-13 02:24:20 -0600

    update C version of converter

--- xyz2cif.c
+++ xyz2cif.c
@@ -13,6 +13,10 @@ int main() {
     char** Element;
     char* symbol;
 
+    double xmax, xmin, ymax, ymin = 0;
+    double xlim, ylim = 0;
+    double theta = 0;
+
     fp = fopen(filename, "r");
     if (fp != NULL){
         while(fgets(buffer, sizeof(buffer), fp) != NULL){
@@ -32,21 +36,32 @@ int main() {
             Element[j] = (char*) malloc(2*sizeof(char));
 
         while(fgets(buffer, sizeof(buffer), fp) != NULL){
+
             sscanf(buffer, "%s %lf %lf %lf",Element[i], &A[i][0], &A[i][1], &A[i][2]);
+            xmin = fmin(xmin,A[i][0]);
+            ymin = fmin(ymin,A[i][1]);
+            zmin = fmin(ymin,A[i][2]);
+            xmax = fmax(xmax,A[i][0]);
+            ymax = fmax(ymax,A[i][1]);
+            zmax = fmax(ymin,A[i][2]);
             printf("%s %lf %lf %lf",Element[i], A[i][0],A[i][1], A[i][2]);
             printf("\n");
 
             i++;
         }
 
-        for(i = 0; i < lines; i++){
+        xlim = xmax - xmin;
+        ylim = ymax - ymin;
+        zlim = zmax - zmin;
+
+        //printf("Xlim %lf Ylim: %lf Zlim: %lf ", xlim, ylim, zlim);
+        for(i = 2; i < lines; i++){
             for(j=0; j < 3; j++) {
-                double tempX =  A[i][0]*0.707 - A[i][1]*0.707;
-                double tempY =  A[i][0]*0.707 + A[i][1]*0.707;
-                A[i][0] = tempX;
-                A[i][1] = tempY;
-                printf("Rotated Data %s %lf %lf %lf",Element[i], A[i][0],A[i][1], A[i][2]);
-                printf("\n");
+                double tempX =  A[i][0]*cos(theta) - A[i][1]*sin(theta)
+                double tempY =  A[i][0]*sin(theta) + A[i][1]*cos(theta)
+                A[i][0] = tempX/xlim;
+                A[i][1] = tempY/ylim;
+                printf("Rotated Data %s %lf %lf %lf\n",Element[i], A[i][0],A[i][1], A[i][2]);
             }
         }
 

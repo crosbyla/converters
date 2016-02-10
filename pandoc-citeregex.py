b1430560e6407f2bbd8bc5ca3ca9289f90d27521
Lawrence Crosby <crosbyla@u.northwestern.edu>
2016-02-10 15:59:30 -0600

    remove crap in functions

--- pandoc-citeregex.py
+++ pandoc-citeregex.py
@@ -3,30 +3,15 @@ import sys
 import re
 import pandocfilters
 from pandocfilters import walk
-from pandocfilters import RawInLine, Str, Para, Plain, Cite
+from pandocfilters import RawInline, Str, Para, Plain, Cite
 
 PATTERN_FIRST = re.compile(r"""(?<={{\s*)(\w+)\s*,(\d+)""",re.VERBOSE)
 PATTERN_SECOND = re.compile(r"""(?<=;\s*)(\w+)\s*,(\d+)""",re.VERBOSE)
 PATTERN_LAST = re.compile(r"""(?<=;\s*)(\w+)\s*,(\d+)\s*}}""",re.VERBOSE)
 
 def processRegex(file_name):
-    with open(file_name) as fp:
-        #read in file
-        for line in fp.readlines():
-            #iterate over lines in file
-            if line.strip():
-                #if flag == False:
-                if re.search(pattern, string):
-                    line.strip()
-                    position.append(line.strip())
-                else :
-                    if re.search(r"""<point name""",line.strip()):
-                        coords.append(line.strip())
-    fp.close()
-
-    file_name += "_filtered.md"
-    fp = open(file_name,'w')
-    fp.close()
-if __name__ == "__main__" :
     processRegex(sys.argv)
-processRegex(filename)
+def main():
+    processRegex()
+if __name__ == "__main__" :
+    main()

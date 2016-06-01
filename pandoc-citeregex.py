6863fcb11e2f9b2ddd9ab792c1094816641d1fcc
Lawrence Crosby <lawrence@Lawrences-MacBook-Pro.local>
2016-06-01 03:25:47 -0500

    working version of code functionality, initial commit

--- pandoc-citeregex.py
+++ pandoc-citeregex.py
@@ -3,12 +3,9 @@ import sys
 import re
 import pandocfilters
 from pandocfilters import walk
-from pandocfilters import RawInline, Str, Para, Plain, Cite
+from pandocfilters import Str, toJSONFilter
 
-PATTERN_FIRST = re.compile(r"""(?<={{\s*)(\w+)\s*,(\d+)""",re.VERBOSE)
-PATTERN_SECOND = re.compile(r"""(?<=;\s*)(\w+)\s*,(\d+)""",re.VERBOSE)
-PATTERN_LAST = re.compile(r"""(?<=;\s*)(\w+)\s*,(\d+)\s*}}""",re.VERBOSE)
-
+PATTERN = re.compile(r"""({{.*}})""",re.VERBOSE)
 
 def subSeperator(string):
     """
@@ -17,10 +14,18 @@ def subSeperator(string):
     """
     return re.sub( r';', r',' , re.sub(r'(\s*,\s*)', r'', string) )
 
+def writeCite(string):
+    return string.replace(r'{{',r'\cite{').replace(r'}}',r'}')
 
-def processRegex(file_name):
-    processRegex(sys.argv)
+def processRegex(key, value, fmt, meta):
+    if key == 'Str':
+        fmt, text = value
+        if fmt == 'latex':
+            if re.search(PATTERN, text):
+                return Str(subSeperator(writeCite(re.search(PATTERN, text).group(1))))
+
 def main():
-    processRegex()
+    toJSONFilter(processRegex)
+
 if __name__ == "__main__" :
     main()

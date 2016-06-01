b87c8792e937c14a962dfc03f6281b06f9c65fa5
Lawrence Crosby <lawrence@Lawrences-MacBook-Pro.local>
2016-06-01 00:46:08 -0500

    add new subSeperator function

--- pandoc-citeregex.py
+++ pandoc-citeregex.py
@@ -9,6 +9,15 @@ PATTERN_FIRST = re.compile(r"""(?<={{\s*)(\w+)\s*,(\d+)""",re.VERBOSE)
 PATTERN_SECOND = re.compile(r"""(?<=;\s*)(\w+)\s*,(\d+)""",re.VERBOSE)
 PATTERN_LAST = re.compile(r"""(?<=;\s*)(\w+)\s*,(\d+)\s*}}""",re.VERBOSE)
 
+
+def subSeperator(string):
+    """
+        input: string (expected pattern {word, number; word, number} i.e a list of word-number pairs seperated by semicolons
+        output: string with semicolons replaced by commas and word, number collapsed to wordnumber
+    """
+    return re.sub( r';', r',' , re.sub(r'(\s*,\s*)', r'', string) )
+
+
 def processRegex(file_name):
     processRegex(sys.argv)
 def main():

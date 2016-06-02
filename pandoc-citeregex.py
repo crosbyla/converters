0f96e2ca3b63af572f6b7a5c96160b86d1ad3ad7
Lawrence Crosby <lawrence@Lawrences-MacBook-Pro.local>
2016-06-01 22:04:08 -0500

    clean slate

--- pandoc-citeregex.py
+++ pandoc-citeregex.py
@@ -1,43 +1,16 @@
 #!/usr/bin/env python3
 import sys
 import re
-from pandocfilters import Str, toJSONFilter, RawInline, Para, walk
+from pandocfilters import Str, toJSONFilter, RawInline, Para, walk, RawBlock
 
 PATTERN1= re.compile(r"""({{.*)""",re.VERBOSE)
 PATTERN2= re.compile(r"""(.*}})""",re.VERBOSE)
 
-def regexSep(string):
-    """
-        input: string (expected pattern {word, number; word, number} i.e a list of word-number pairs seperated by semicolons
-        output: string with semicolons replaced by commas and word, number collapsed to wordnumber
-    """
-    return re.sub( r';', r',' , re.sub(r'(\s*,\s*)', r'', string) )
-
-def regexCite(string):
-    return re.sub(r'{{',r'\cite{', re.sub( r'}}', r'}', string) )
 
 def processCite(key, value, fmt, meta):
     if key == 'Para':
-        for n, a in enumerate(value):
-            if a['t'] == 'Str' :
-                m = re.search(PATTERN1, a['c'])
-                b = value[(n+1)%len(value)]
-                if m and b['t'] == 'Space':
-                    sys.stderr.write(str(m.group(1)))
-                m = re.search(PATTERN2, a['c'])
-                if m:
-                    sys.stderr.write(str(m.group(1)))
-
-
-        if key == 'Str' and re.search(PATTERN1, value):
-            return RawInline('latex', subSeperator(writeCite(re.search(PATTERN1, value).group(1))))
-            #if fmt == 'latex':
-        elif key == 'Str' and re.search(PATTERN1, value):
-            return RawInline('latex', subSeperator(writeCite(re.search(PATTERN2, value).group(1))))
-
-def removeSpace(key, value, fmt, meta):
-    if key == 'Space':
-        return Str("")
+        pass
+    return None
 
 def main():
     toJSONFilter(processCite) # pass 1

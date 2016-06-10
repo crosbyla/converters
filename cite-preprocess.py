6c9b7c640ce1ccea72158e1f7523facdd5a1e4ea
Lawrence Crosby <crosbyla2012@gmail.com>
2016-06-09 21:45:52 -0500

    default to stdout

--- cite-preprocess.py
+++ cite-preprocess.py
@@ -15,20 +15,25 @@ def regexCite(string):
     return re.sub(r'{{\s*',r'~\cite{', re.sub( r'\s*}}',r'}', string) )
 
 def processFile(file_name, outfile = None):
-    if outfile is None:
-       outfile = file_name
 
     with open(file_name) as fp:
         f = fp.read()
 
     lines = f.split('\n')
-    with open(outfile, 'w') as fp:
+    if outfile:
+        with open(outfile, 'w') as fp:
+            for line in lines:
+                fp.write(processLine(line) + '\n')
+    else :
         for line in lines:
-            m = re.search(PATTERN,line)
-            if m:
-               subst = regexCite(regexSep(m.group(1)))
-               line = line[:m.start(1)] + subst + line[m.end(1):]
-            fp.write(line + '\n')
+            sys.stdout.write(processLine(line) + '\n')
+
+def processLine(line):
+    m = re.search(PATTERN,line)
+    if m:
+        subst = regexCite(regexSep(m.group(1)))
+        line = line[:m.start(1)] + subst + line[m.end(1):]
+    return line
 
 def main():
     if len(sys.argv) == 2:
